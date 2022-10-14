ea9e5f0dafbe0f80f8d6cbda5f59cdccae61d595
Long Zhao <82784174+LongZecs36b@users.noreply.github.com>
2022-10-14 00:07:16 -0700

    Update sshell.c

--- sshell.c
+++ sshell.c
@@ -315,7 +315,7 @@ void my_pushd(char *arg)
             ptr->next = head;
             head = ptr;
         }
-        printf("%s\n", getcwd(NULL, 0));
+        // printf("%s\n", getcwd(NULL, 0));
         fprintf(stderr, "+ completed 'pushd %s' [0]\n", arg);
     }
 }
